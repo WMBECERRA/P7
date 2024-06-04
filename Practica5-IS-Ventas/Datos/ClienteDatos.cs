eadeada6895b8743882e6f668b345d93dc9bd6cd
WMBECERRA <wmbecerra@puce.edu.ec>
2024-06-03 22:54:10 -0500

    Initial commit

---
+++ Practica5-IS-Ventas/Datos/ClienteDatos.cs
@@ -0,0 +1,39 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class ClienteDatos : IDatos<Cliente>
+    {
+        servicios1Entities1 contexto;
+        ClienteDatos()
+        {
+            contexto = new servicios1Entities1();
+        }
+        public bool Actualizar(Cliente item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Eliminar(Cliente item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> Listar()
+        {
+            return contexto.Cliente.ToList();
+        }
+
+        public bool Nuevo(Cliente item)
+        {
+            contexto.Cliente.Add(item);
+            contexto.SaveChanges();
+            return true;
+        }
+    }
+}
